f71bb58d49e3bc64be092dd3537ffbb4a1f8a8d6
Ali Dhuniya <alihussaindhuniya0307@gmail.com>
2020-10-08 12:49:43 -0700

    singleProduct components styled

--- src/components/SingleProduct.jsx
+++ src/components/SingleProduct.jsx
@@ -15,19 +15,23 @@ import { AddCart} from "../redux/actions/allProducts";
 
             const product = items ? (
                  <Fragment>
-                 <h2>single Product List</h2>
+                 <h2 className = "products-heading">single Product List</h2>
     
-    <h1>Title:{items.fields.offertitle}</h1>
-   <img style = {{width: "200px"}} src ={items.fields.offerimg.fields.file.url} alt="product-img" />
-    <h5>Description:{ items.fields.offerdec}</h5>
-    <p>Price:{  items.fields.offerprice}</p>
-    <span
-            className="badge badge-primary"
-            style={{ cursor: "pointer" }}
-            onClick={() => this.props.AddCart(items)}
-          >
-            Add Cart
-          </span>
+                 <div className = "products">
+                    <div className="products__id"> 
+                    <img className = "products__img"  src ={items.fields.offerimg.fields.file.url} alt="product-img" />
+                    <div className = "products__content">
+                  <h1 className = "products__h1">Title:{items.fields.offertitle}</h1>
+                 
+                  <h5 className = "products__h5">Description:{items.fields.offerdec}</h5>
+                  <p className = "products__p">Price: ${items.fields.offerprice} <span
+                  className = "products__span"
+                    onClick={() => this.props.AddCart(items)}  >
+                    Add to Cart
+                  </span></p>
+                  </div>
+                  </div>
+                  </div>
                  </Fragment>
              ) : (
                <div >Loading singleProduct...</div>
