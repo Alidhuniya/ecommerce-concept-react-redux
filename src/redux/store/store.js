b3082963e15dfe6cd52eccdf0553d80867dacf60
Ali Dhuniya <alihussaindhuniya0307@gmail.com>
2020-10-08 13:47:07 -0700

    redux dev tool removed

--- src/redux/store/store.js
+++ src/redux/store/store.js
@@ -2,16 +2,11 @@
 import thunk from 'redux-thunk';
 import { createStore, applyMiddleware, compose } from 'redux';
 import allReducers from "./../reducers/index";
-// import logger from 'redux-logger';
-
-// const immutable = process.env.NODE_ENV !== 'production' ?
-//   [require('redux-immutable-state-invariant').default(), thunk] :
-//   [thunk];
 
 
 const store = createStore(
     allReducers,
-    compose(applyMiddleware(thunk),   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
+    compose(applyMiddleware(thunk))
   )
 
   export default store;
